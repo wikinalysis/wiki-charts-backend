23f3769c709e3bc488691cc2d75ca32d31974a1d
Kevin Rocker <kevin.j.rocker@gmail.com>
2019-12-07 21:12:30 -0500

    Upload Script

---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_TAG=wiki-backend
+
+docker build -t $IMAGE_TAG .
+container_id=$(docker create $IMAGE_TAG)
+docker cp ${container_id}:/app/start_release start_release
+docker rm ${container_id}
+gsutil cp start_release gs://wikinalysis-production-releases/release
+rm start_release
